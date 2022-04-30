33d6e6cd9ad3bce5a1e5abfebee7783ce4f08885
AlexanderLyon <alyon011@gmail.com>
2022-04-30 11:12:37 -0400

    Add TypeScript

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from '../images/logo.svg';
 
-export const App = () => {
+export const App: React.FC = () => {
   return (
     <main className="app">
       <img className="logo" src={logo} alt="Splitter" />
