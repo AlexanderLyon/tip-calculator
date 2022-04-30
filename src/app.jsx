76823657e059caff03a4c8a4daa0bd7d1e4f6e3b
AlexanderLyon <alyon011@gmail.com>
2022-04-30 10:37:15 -0400

    Add Logo and base styles

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+export const App = () => {
+  return (
+    <main className="app">
+      <img className="logo" src={logo} alt="Splitter" />
+    </main>
+  );
+};
